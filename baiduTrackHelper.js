6f5222a477b8776d9812d0254cd8f0eeedcce38d
Tony Mao <huiliumao@gmail.com>
2013-09-29 12:33:57 +0800

    Add minified js file,edit license file

--- baiduTrackHelper.js
+++ baiduTrackHelper.js
@@ -1,3 +1,7 @@
+/*
+Apache License
+author: Tony Mao [huiliumao@gmail.com]
+*/
 try{
 	jQuery.noConflict();
 }catch(e){
@@ -7,8 +11,23 @@ var _hmt = _hmt || [];
 if(typeof _BTA != 'undefined' && _BTA != ''){
 	var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
 	document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3F" + _BTA + "' type='text/javascript'%3E%3C/script%3E"));
-	// (function($){
-	// })(jQuery);
+	(function($){
+		$(function(){
+			$('a.baiduTrack').click(function(e){
+				e.preventDefault();
+				var baiduTrack = $(this).attr('data-baiduTrack');
+				if (baiduTrack){
+					_hmt.push(eval(baiduTrack));
+				}
+				var rel = $(this).attr('rel');
+				if (rel == 'new'){
+					window.open($(this).attr('href'));
+				}else{
+					window.location.href = $(this).attr('href');
+				}
+			});
+		});
+	})(jQuery);
 }else{
 	alert('Must set the _BTA variable first!');
 }
